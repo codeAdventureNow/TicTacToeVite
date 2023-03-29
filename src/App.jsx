f53efe906ccc1d71d3657a421feb25c75ca99227
[David <[nmueller855@gmail.com]>
2023-03-29 15:19:14 -0700

    Tic Tac Toe before refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function Square({ value, onSquareClick }) {
@@ -148,7 +147,6 @@ function App() {
     setComputerTurn(false);
     setAvailableSquares(allSquaresOpen);
   }
-
   return (
     <div className='App'>
       <h1>
